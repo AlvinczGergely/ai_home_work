ca037a4a48d27b0078783337eb91b1ab7c1ac518
Gergely Alvincz <alvincz.gergely@gmai.com>
2026-04-11 21:51:38 +0200

    fix both paddle stuck problem

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def train_ai(genome1, genome2, config, window, width, height):
     #game.draw()
     #pygame.display.update()
     
-    if game_info.left_score >= 1 or game_info.right_score >=1 or game_info.left_hits > 50:
+    if game_info.left_score >= 1 or game_info.right_score >= 1 or game_info.left_hits + game_info.right_hits > 50:
       calculate_fitness(genome1, genome2, game_info)
       break
 
